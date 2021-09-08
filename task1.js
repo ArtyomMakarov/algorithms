0c0ec5d2c798d4460b0a43107dca8e1f3523266f
Artyom Makarov <artyom.makarov@cortlex.com>
2021-09-08 11:29:18 +0300

    feat: add breadth first search  and dijkstra algorithms

--- task1.js
+++ task1.js
@@ -85,5 +85,168 @@ function selectionSort(arr) {
 selectionSort([10,5,2,3]);
 
 
+// Алгоритм поиска в ширину. Вычисляет кратчайший путь в невзвешенном графе.
+
+let tree = {
+	"10": {
+		value: "10",
+		left: "4",
+		right: "17",
+	},
+	"4": {
+		value: "4",
+		left: "1",
+		right: "9",
+	},
+	"17": {
+		value: "17",
+		left: "12",
+		right: "18",
+	},
+	"1": {
+		value: "1",
+		left: null,
+		right: null,
+	},
+	"9": {
+		value: "9",
+		left: null,
+		right: null,
+	},
+	"12": {
+		value: "12",
+		left: null,
+		right: null,
+	},
+	"18": {
+		value: "18",
+		left: null,
+		right: null,
+	},
+};
+
+let BreadthFirstSearch = (tree, rootNode, searchValue) => {
+	// make a queue array
+	let queue = [];
+	// populate it with the node that will be the root of your search
+	queue.push(rootNode);
+
+	// search the queue until it is empty
+	while (queue.length > 0) {
+	// assign the top of the queue to variable currentNode
+		let currentNode = queue[0];
+		console.log("Current node is:" + currentNode.value);
+
+		// if currentNode is the node we're searching for, break & alert
+		if (currentNode.value === searchValue) {
+			console.log("Found it!");
+			return;
+		}
+
+		// if currentNode has a left child node, add it to the queue.
+		if (currentNode.left !== null) {
+			queue.push(tree[currentNode.left]);
+		}
+
+		// if currentNode has a right child node, add it to the queue.
+		if (currentNode.right !== null) {
+			queue.push(tree[currentNode.right]);
+		}
+		// remove the currentNode from the queue.
+		queue.shift();	
+    }
+	console.log("Sorry, no such node found :(");	
+}
+
+BreadthFirstSearch(tree, tree[10], "12");
+
+
+
+
+
+// Алгоритм Дейкстры. Позволяет найти самый "дешёвый" путь в направленном, взвешенном графе.
+
+const problem = {
+	start: {A: 5, B: 2},
+	A: {C: 4, D: 2},
+	B: {A: 8, D: 7},
+	C: {D: 6, finish: 3},
+	D: {finish: 1},
+	finish: {}
+  };
+  
+  const lowestCostNode = (costs, processed) => {
+	return Object.keys(costs).reduce((lowest, node) => {
+	  if (lowest === null || costs[node] < costs[lowest]) {
+		if (!processed.includes(node)) {
+		  lowest = node;
+		}
+	  }
+	  return lowest;
+	}, null);
+  };
+  
+  // function that returns the minimum cost and path to reach Finish
+  const dijkstra = (graph) => {
+  
+	// track lowest cost to reach each node
+	const costs = Object.assign({finish: Infinity}, graph.start);
+  
+	// track paths
+	const parents = {finish: null};
+	for (let child in graph.start) {
+	  parents[child] = 'start';
+	}
+  
+	// track nodes that have already been processed
+	const processed = [];
+  
+	let node = lowestCostNode(costs, processed);
+  
+	while (node) {
+	  let cost = costs[node];
+	  let children = graph[node];
+	  for (let n in children) {
+		  let newCost = cost + children[n];
+		if (!costs[n]) {
+		  costs[n] = newCost;
+		  parents[n] = node;
+		}
+		if (costs[n] > newCost) {
+		  costs[n] = newCost;
+		  parents[n] = node;
+		}
+	  }
+	  processed.push(node);
+	  node = lowestCostNode(costs, processed);
+	}
+  
+	let optimalPath = ['finish'];
+	let parent = parents.finish;
+	while (parent) {
+	  optimalPath.push(parent);
+	  parent = parents[parent];
+	}
+	optimalPath.reverse();
+  
+	const results = {
+	  distance: costs.finish,
+	  path: optimalPath
+	};
+  
+	return results;
+  };
+  
+  console.log(dijkstra(problem));
+
+  
+
+
+
+
+
+
+
+
 
 
